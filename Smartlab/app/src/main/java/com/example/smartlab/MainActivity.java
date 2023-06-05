9192629f0f23aefc8e990fad1a3ee634b43e469d
Бородин Иван Сергеевич <pr110borodin@oat.local>
2023-06-05 11:50:13 +0600

    Loading screen added

--- Smartlab/app/src/main/java/com/example/smartlab/MainActivity.java
+++ Smartlab/app/src/main/java/com/example/smartlab/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.smartlab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                Intent intent = new Intent(MainActivity, Sign.class);
+                startActivity(intent);
+            }
+
+
+        };
     }
 }
